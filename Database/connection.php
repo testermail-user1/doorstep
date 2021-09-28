dd1fda3ccd2b72ef08e2720e1b72e4a3f31c7acc
Pavanvid <pavan.bandi10@gmail.com>
2021-09-27 20:00:33 -0400

    required

--- Database/connection.php
+++ Database/connection.php
@@ -1,10 +1,19 @@
 <?php
     //PDO -- for security
-    $host = 'localhost';  //or ip 127.0.0.1
-    $db = 'door-step_db';
-    $user = 'root';   //as a user
-    $pass = '';   //password
-    $charset = 'utf8mb4';  
+    //$host = 'localhost';  //or ip 127.0.0.1
+    //$db = 'door-step_db';
+    //$user = 'root';   //as a user
+    //$pass = '';   //password
+    //$charset = 'utf8mb4'; 
+    
+    // remote part
+    $host = 'sql6.freesqldatabase.com';  //or ip 127.0.0.1
+    $db = 'sql6440618';
+    $user = 'sql6440618';   //as a user
+    $pass = 'Izcr8LmtJa';   //password
+    $charset = 'utf8mb4'; 
+    
+    
     $dsn = "mysql:host=$host;dbname=$db;chartset=$charset";
     try{
         $pdo = new PDO($dsn,$user,$pass);
